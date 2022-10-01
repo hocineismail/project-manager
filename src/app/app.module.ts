e66fae6734ab3392be1828acc6699e3aa98526de
hocineismail <hocineismail75@gmail.com>
2022-10-01 15:00:10 +0100

    fetch projects data using http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectComponent } from './components/project/project.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
